bf70b97563373d1ededd3818b1a529f1cbc9a346
Christopherchinia <chinia214@gmail.com>
2022-09-28 05:28:53 +0300

    commit tree 355e0121c44b9e2df10621e9ccd194ae39ee4590

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Taxis
+taxi1 = Taxi.create(name: "movers and shakers")
+taxi2 = Taxi.create(name: "final destination")
+taxi3 = Taxi.create(name: "safety by all means")
+taxi4 = Taxi.create(name: "guaranteed services")
+
+# passengers
+passenger1 = Passenger.create(name: "Laura Kimbley")
+passenger2 = Passenger.create(name: "Troy Martin")
+passenger3 = Passenger.create(name: "Denzel Curry")
+passenger4 = Passenger.create(name: "John Doe")
+
+# Ride 
+Ride.create(taxi_id: taxi1.id, passenger_id: passenger4.id)
+Ride.create(taxi_id: taxi2.id, passenger_id: passenger3.id)
+Ride.create(taxi_id: taxi3.id, passenger_id: passenger2.id)
+Ride.create(taxi_id: taxi4.id, passenger_id: passenger1.id)
+Ride.create(taxi_id: taxi3.id, passenger_id: passenger2.id)
+Ride.create(taxi_id: taxi1.id, passenger_id: passenger3.id)
+Ride.create(taxi_id: taxi2.id, passenger_id: passenger1.id)
+Ride.create(taxi_id: taxi4.id, passenger_id: passenger4.id)
+
+puts "...Done Seeding."
+
